42e2878b2c9adb90d2343c2a4ac3606ea7b8ab9e
Jibran Arshad <ranajibranarshad@gmail.com>
2025-07-31 00:30:30 +0500

    Add Authentication middleware in routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,14 +1,15 @@
 import express from "express";
 import { createUser, deleteUser, getAllUsers, getOtherUsers, getUserById, loginUser, updateUser } from "../app/controllers/userController/index.js";
+import { checkAuth } from "../app/middlewares/handlers/checkAuth.js";
 
 const router = express.Router();
 
-router.post("/create", createUser);
+router.post("/create",  createUser);
 router.post("/login", loginUser);
-router.get("/get-all", getAllUsers);
-router.get("/get-by-id/:id", getUserById);
-router.put("/update/:id", updateUser);
-router.delete("/delete/:id", deleteUser);
-router.get("/get-other-users/:id", getOtherUsers);
+router.get("/get-all", checkAuth, getAllUsers);
+router.get("/get-by-id/:id",checkAuth, getUserById);
+router.put("/update/:id", checkAuth, updateUser);
+router.delete("/delete/:id", checkAuth, deleteUser);
+router.get("/get-other-users/:id", checkAuth, getOtherUsers);
 
 export default router;
