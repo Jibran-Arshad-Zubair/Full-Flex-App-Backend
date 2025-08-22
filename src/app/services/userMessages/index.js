0bb2d4d65fbf05d861a650c37599a1f39536986c
Jibran Arshad <ranajibranarshad@gmail.com>
2025-08-22 23:49:39 +0500

    Fixes

--- src/app/services/userMessages/index.js
+++ src/app/services/userMessages/index.js
@@ -50,7 +50,7 @@ export async function sendMessageService(senderId, receiverId, message) {
 
 export async function getMessageService(senderId, receiverId) {
   try {
-    console.log("senderId",senderId,"receiverId",receiverId);
+    // console.log("senderId",senderId,"receiverId",receiverId);
     if (!isValidId(senderId) || !isValidId(receiverId)) {
       return {
         status: 400,
