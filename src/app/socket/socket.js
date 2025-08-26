fab61a27ce9325ab948b82cae755a83611b30416
Jibran Arshad <ranajibranarshad@gmail.com>
2025-08-26 23:27:31 +0500

    Created handShake from backend to send online users

--- src/app/socket/socket.js
+++ src/app/socket/socket.js
@@ -12,11 +12,28 @@ const io = new Server(server, {
     },
 });
 
+const userSocketMap = {};  // Here i am creating a map to store user IDs and socket IDs
+
 io.on('connection', (socket) => {
     console.log('User connected', socket.id);
 
+    const userId = socket.handshake.query.userId;
+    if (userId) {
+        userSocketMap[userId] = socket.id; // Here i am storing the mapping of userId to socket.id
+    }
+
+    io.emit('getOnlineUsers', Object.keys(userSocketMap)); // Here i am using the emit to send data from backend to frontend
+
     socket.on('disconnect', () => {
         console.log('User disconnected', socket.id);
+       
+        const disconnectedUserId = Object.keys(userSocketMap).find(
+            key => userSocketMap[key] === socket.id
+        );
+        if (disconnectedUserId) {
+            delete userSocketMap[disconnectedUserId];
+            io.emit('getOnlineUsers', Object.keys(userSocketMap));
+        }
     });
 
     socket.on('error', (error) => {
